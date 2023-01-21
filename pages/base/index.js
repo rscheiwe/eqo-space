55b69776ce47a6d8df90f0d8805c4304d98163a7
rscheiwe <rscheiwe@gmail.com>
2023-01-20 20:14:45 -0500

    new

--- pages/base/index.js
+++ pages/base/index.js
@@ -2,7 +2,6 @@ import styles from "@/styles/Home.module.css";
 import { Inter } from '@next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
-
 export default function App() {
   return (
     <>
